8187ad60e9177b41c691177a3444d261830d5b9d
Evgeniy Choopin <evgrafiy@outlook.com>
2022-11-29 20:51:52 +0300

    Проверим

--- 064/Program.cs
+++ 064/Program.cs
@@ -2,4 +2,28 @@
 // N = 5 -> "4, 2"
 // N = 8 -> "8, 6, 4, 2,"
 
+Console.Write("Загадайте чёткое натуральное число, на котором всё закончится: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+EvenToLow(number, count);
+int i = 1;
+while (i <= numNum)
+{
+    if(i % 2 == 0){
+        Console.WriteLine(i);
+    }
+    i++;
+}
 
+void EvenToLow(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        EvenToLow(n, count + 1);
+        Console.Write(count + " ");
+    }
+}
